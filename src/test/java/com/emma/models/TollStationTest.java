c1d442197171430e96df5462235007c0374f969a
Emma <emmalanza@hotmail.com>
2025-03-31 14:53:04 +0200

    test toll station print method: failed

--- src/test/java/com/emma/models/TollStationTest.java
+++ src/test/java/com/emma/models/TollStationTest.java
@@ -2,16 +2,19 @@ package com.emma.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
 
 public class TollStationTest {
 
     @Test
     void testTollStationCollection() {
-        
+
         TollStation tollStation = new TollStation("Central", "Springfield");
         Car car = new Car("ABC123");
-        Motorbike motorbike = new Motorbike("ABC123");  
+        Motorbike motorbike = new Motorbike("ABC123");
         Truck truck = new Truck("ABC123", 4);
         tollStation.addVehicle(car);
         tollStation.addVehicle(motorbike);
@@ -19,6 +22,28 @@ public class TollStationTest {
 
         assertEquals(3, tollStation.getVehicles().size());
         assertEquals(350, tollStation.getTotalCollected());
+    }
+
+    @Test
+    void testPrint() {
+        TollStation tollStation = new TollStation("Central", "Springfield");
+        Truck truck = new Truck("ABC123", 2);
+        tollStation.addVehicle(truck);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        tollStation.print();
+
+        String expectedOutput = "Toll Station: Central in Springfield\n" +
+                "Registered Vehicles:\n" + 
+                "- ABC123 (Toll: $100.0)\n" +
+                "Total Toll Collected: $100.0\n";
+
+        assertEquals(expectedOutput, outputStream.toString());
+
+        System.setOut(System.out);
     }
 
 }
