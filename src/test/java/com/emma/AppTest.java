4529446c7fd2387f1c0f0d5db5e161206370808f
Emma <emmalanza@hotmail.com>
2025-03-31 12:54:33 +0200

    inital commit

---
+++ src/test/java/com/emma/AppTest.java
@@ -0,0 +1,14 @@
+package com.emma;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+
+}
